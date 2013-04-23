82c496a6ca3be7ac3ef23b5862c8fbd8848a9172
Ross Harrison <rtharrison86@gmail.com>
2013-04-23 15:15:21 -0500

    can call with arguments

--- jasmineSharedExamples.js
+++ jasmineSharedExamples.js
@@ -3,10 +3,21 @@ var sharedExamples = {}
 /*
  * Bind new shared example
  */
-function sharedExampleFor(label, specFunction){
-  sharedExamples[label] = specFunction;
+function sharedExampleFor(exampleName, specFunction){
+  sharedExamples[exampleName] = specFunction;
 }
 
-function itBehavesLike(label){
-  sharedExamples[label]()
+/*
+ * Calls the exampleName passed with the arguments sent
+ */
+function itBehavesLike(exampleName){
+  var args = [];
+  if(arguments.length > 1){
+    var args = Array.prototype.slice.call(arguments, 1);
+  }
+  
+  //call the example with the exampleName and passed arguments
+  describe("It behaves like "+exampleName, function(){
+    sharedExamples[exampleName].apply(this, args)
+  });
 }
--- spec.js
+++ spec.js
@@ -1,16 +1,27 @@
-describe("jasmineSharedExamples", function(){
-  beforeEach(function(){
-    this.specFun = function(){};
-    spyOn(this, "specFun")
-    sharedExampleFor("foo", this.specFun)
-  });
-  
-  it("sharedExampleFor registers the function to the right label", function(){
-    expect(sharedExamples["foo"]).toBe(this.specFun);
-  });
+var specFun = jasmine.createSpy()
+sharedExampleFor("foo", specFun)
 
-  it("itBehavesLike calls the right spec function", function(){
-    itBehavesLike("foo");
-    expect(this.specFun).toHaveBeenCalled();
+describe("jasmineSharedExamples", function(){
+  describe("sharedExampleFor", function(){
+    it("sharedExampleFor registers the function to the right label", function(){
+      expect(sharedExamples["foo"]).toBe(specFun);
+    });
+  });    
+
+  describe("itBehavesLike", function(){
+      
+    describe("no arguments", function(){
+      itBehavesLike("foo");
+      it("calls the example", function(){
+        expect(specFun).toHaveBeenCalled();
+      });
+    });
+
+    describe("with arguments", function(){
+      itBehavesLike("foo", 1,2,3,4,5);
+      it("calls the example with arguments", function(){
+        expect(specFun).toHaveBeenCalledWith(1,2,3,4,5);
+      });        
+    });
   });
 });
